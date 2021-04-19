911166c036cf3e0631c50b2f3026b402b401a381
Hephzi <52066666+hephzibahponcellat@users.noreply.github.com>
2021-04-19 11:30:17 +0530

    initial skeleton

---
+++ snake/main.py
@@ -0,0 +1,27 @@
+def main():
+
+    # initial position for snake with food
+
+    # start once the user press any arrow keys
+    # once any arrow key pressed, start the game
+
+    """
+    while game is on
+        # move the snake forward
+
+        # check if the user pressed any arrow key
+            # if yes, turn accordingly
+
+        # check if it has ate the food
+            # if yes, add score, 
+            # grow the snake, 
+            # create new food
+
+        # check if it has hit the wall
+            # if yes, end game
+
+    # update score board
+    """
+
+if __name__ == "__main__":
+    main()
