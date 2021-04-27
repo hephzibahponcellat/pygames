26d12f22add04a89f6122181bb485ee146758a09
Hephzi <52066666+hephzibahponcellat@users.noreply.github.com>
2021-04-27 16:57:35 +0530

    Update main.py

--- snake/main.py
+++ snake/main.py
@@ -9,21 +9,25 @@ def main():
 
     # initial position for snake with food
     x1y1, x2y2, snake_len = snake_initial_position(screen)
+    print(x1y1, x2y2, snake_len)
 
     # once any arrow key pressed, start the game
     game_on, snake_speed, key_pressed = start_game()
     print(game_on, snake_speed, key_pressed)
 
+    clock = pygame.time.Clock()
+
     # while game is on
     while game_on:
+        clock.tick(snake_speed)
         # move the snake
         x1y1, x2y2 = move_snake(snake_speed, key_pressed,
                                 x1y1, x2y2, screen, snake_len)
+        print("#", x1y1, x2y2)
 
         # check if it has hit the wall
         # if yes, end game
         hit_wall = did_hit_wall(x1y1, x2y2)
-        print(x1y1, x2y2)
         if hit_wall:
             sys.exit()
 
