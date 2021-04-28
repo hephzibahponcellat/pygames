befcd51fe1ee31af5dd998797871c340ed42a154
Hephzi <52066666+hephzibahponcellat@users.noreply.github.com>
2021-04-28 15:47:11 +0530

    Update main.py

--- snake/main.py
+++ snake/main.py
@@ -21,8 +21,9 @@ def main():
         clock.tick(snake_speed)
 
         # move the snake
-        x1y1, x2y2 = move_snake(snake_speed, key_pressed, snake_cur_dir,
+        x1y1, x2y2, snake_cur_dir = move_snake(snake_speed, key_pressed, snake_cur_dir,
                                 x1y1, x2y2, screen, snake_len)
+        key_pressed = ""
         print("#", x1y1, x2y2)
 
         # check if it has hit the wall
@@ -41,6 +42,15 @@ def main():
 
             # check if the user pressed any arrow key
                 # if yes, turn accordingly
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    key_pressed = "right"
+                elif event.key == pygame.K_LEFT:
+                    key_pressed = "left"
+                elif event.key == pygame.K_UP:
+                    key_pressed = "up"
+                elif event.key == pygame.K_DOWN:
+                    key_pressed = "down"
 
             # check if it has ate the food
                 # if yes, add score,
