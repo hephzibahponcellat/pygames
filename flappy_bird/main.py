8669c4010b118811d03b7a99036a2ac25ea13ef6
Hephzibah Pon Cellat Arulprakash <poncellat@gmail.com>
2022-07-12 17:20:00 +0400

    check bird hit. stop game if yes.

--- flappy_bird/main.py
+++ flappy_bird/main.py
@@ -2,15 +2,14 @@ from flappy_bird import *
 
 
 def play_game(screen, bg, bird, key_pressed):
-    bg.update_background(screen)
-    bird.fly_up_down(screen, key_pressed)
-
     # check if bird hit, yes,
     # crash bird to ground
     # stop background animation
     if bird.is_hit(screen):
         pass
-
+    else:
+        bg.update_background(screen)
+        bird.fly_up_down(screen, key_pressed)
 
 def game_not_started(screen, bg, bird):
     bg.update_background(screen)
