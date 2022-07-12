780d46ea368fe789c97d0d43c3c34b06de20cc2a
Hephzibah Pon Cellat Arulprakash <poncellat@gmail.com>
2022-07-12 15:45:43 +0400

    adding img, using constants, bird motions

--- flappy_bird/flappy_bird.py
+++ flappy_bird/flappy_bird.py
@@ -19,6 +19,8 @@ class Background(GameScreen):
 
     def __init__(self, game_screen):
         self.gs = game_screen
+        self.delay_ms = 200
+
         self.bg_img = 'images/bg.png'
         self.fence_img = 'images/fence.png'
 
@@ -46,34 +48,63 @@ class Background(GameScreen):
         if self.fences[0]['x'] + fence_w < 0:
             self.fences.pop(0)
 
+    def update_display(self):
+        pygame.display.flip()
+        pygame.time.delay(self.delay_ms)
+
 
 class Bird(GameScreen):
     def __init__(self, game_screen):
+        self.BIRD_INITIAL_ANGLE = 0
+        self.BIRD_MAX_UP_ANGLE = 20
+        self.BIRD_MAX_DOWN_ANGLE = -90
+        self.BIRD_CHANGE_ANGLE = 30
+        self.BIRD_INITIAL_YPOS = 200
+        self.BIRD_Y = 40
+        self.BIRD_XPOS = 50
+
         self.gs = game_screen
-        self.bird_pos = {'x': 50, 'y': 200}
+        self.bird_pos = {'x': self.BIRD_XPOS, 'y': self.BIRD_INITIAL_YPOS}
+        self.bird_angle = self.BIRD_INITIAL_ANGLE
         self.wing_up = True
 
+        self.bird_img = 'images/bird.png'
         self.bird_up_img = 'images/bird_up.png'
         self.bird_down_img = 'images/bird_down.png'
 
+        self.bird = pygame.image.load(self.bird_img)
         self.bird_up = pygame.image.load(self.bird_up_img)
         self.bird_down = pygame.image.load(self.bird_down_img)
 
     def fly(self, screen):
         if self.wing_up:
             self.wing_up = False
-            screen.blit(self.bird_up, (self.bird_pos['x'], self.bird_pos['y']))
+            b = pygame.transform.rotate(self.bird_up, self.bird_angle)
         else:
             self.wing_up = True
-            screen.blit(self.bird_down, (self.bird_pos['x'], self.bird_pos['y']))
+            b = pygame.transform.rotate(self.bird_down, self.bird_angle)
+
+        if self.bird_angle <= self.BIRD_MAX_DOWN_ANGLE:
+            b = pygame.transform.rotate(self.bird, self.bird_angle)
+
+        screen.blit(b, (self.bird_pos['x'], self.bird_pos['y']))
 
     def fly_up_down(self, screen, key_pressed):
         # if key pressed move bird up
         # else move bird down
         if key_pressed:
-            self.bird_pos['y'] -= 20
+            self.bird_pos['y'] -= (self.BIRD_Y * 2)
+            self.bird_angle = self.BIRD_MAX_UP_ANGLE
+
         else:
-            self.bird_pos['y'] += 20
+            self.bird_pos['y'] += self.BIRD_Y
+
+            # gradually turn bird down till -90
+            if self.bird_angle > self.BIRD_MAX_DOWN_ANGLE:
+                self.bird_angle -= self.BIRD_CHANGE_ANGLE
+            else:
+                self.bird_pos['y'] += self.BIRD_Y
+                self.bird_angle = self.BIRD_MAX_DOWN_ANGLE
 
         self.fly(screen)
 
